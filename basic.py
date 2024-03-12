5b0462b68ca3c98050f991a2d5423f3bef54ecc4
2531Chuku <159626434+2531Chuku@users.noreply.github.com>
2024-03-12 22:00:35 +0200

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,21 @@
+# declare the variable
+personName = 'Pearl'
+print(personName);
+
+#-------------data type----------
+age = 12;
+isGraduated = True
+
+# ----- if statement, Arrays and Loops ----------
+if(5>3):
+  {
+    print('Success')
+  }
+
+def play():{
+  print('hello')
+}
+play()
+
+name = 'Jane'
+
